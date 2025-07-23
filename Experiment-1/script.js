0f6b6720819484856554a15353f1428d68acda3f
Atharv Sharma <23bai70235@cuchd.in>
2025-07-23 16:51:53 +0000

    commit

--- Experiment-1/script.js
+++ Experiment-1/script.js
@@ -1,9 +1,20 @@
+document.getElementById("name").addEventListener("keypress", function(e) {
+  if (/\d/.test(e.key)) {
+    e.preventDefault();
+  }
+});
+
 document.getElementById("registrationForm").addEventListener("submit", function(e) {
-    e.preventDefault(); // Prevent form from submitting
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const age = document.getElementById("age").value;
-  
-    alert(`Name: ${name}\nEmail: ${email}\nAge: ${age}`);
-  });
-  
+  e.preventDefault(); 
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const age = document.getElementById("age").value;
+  if (!name || !email || !age) {
+    alert("All fields are required!");
+    return;
+  }
+
+  alert(`Name: ${name}\nEmail: ${email}\nAge: ${age}`);
+  console.log("Registered successfully");
+});
